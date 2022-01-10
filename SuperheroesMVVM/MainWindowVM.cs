44908a02b8cf372fbbb3b9b2aaf0109ee6c9099d
Pablo <pablo.martinezaznar@iesdoctorbalmis.com>
2022-01-10 14:24:35 +0100

    Agregar archivos de proyecto.

---
+++ SuperheroesMVVM/MainWindowVM.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using SuperheroesMVVM.servicios;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperheroesMVVM
+{
+    class MainWindowVM : ObservableObject
+    {
+
+        //Propiedades
+        public List<Superheroe> heroes;
+
+        private Superheroe heroeActual;
+
+        public Superheroe HeroeActual
+        {
+            get { return heroeActual; }
+            set 
+            {
+                SetProperty(ref heroeActual, value);
+            }
+        }
+
+        private int total;
+
+        public int Total
+        {
+            get { return total; }
+            set 
+            {
+                SetProperty(ref total, value);
+            }
+        }
+
+        private int actual;
+
+        public int Actual
+        {
+            get { return actual; }
+            set 
+            { 
+                SetProperty(ref actual, value);
+            }
+        }
+
+        //Comandos
+        public RelayCommand AvanzarCommand { get; }
+
+        public RelayCommand RetrocederCommand { get; }
+
+        //servicios
+        public SuperheroesService superheroesService;
+
+
+        public MainWindowVM()
+        {
+            superheroesService = new SuperheroesService();
+            heroes = superheroesService.GetSamples();
+            HeroeActual = heroes[0];
+            Total = heroes.Count;
+            Actual = 1;
+            AvanzarCommand = new RelayCommand(Siguiente);
+            RetrocederCommand = new RelayCommand(Anterior);
+        }
+
+        public void Siguiente()
+        { 
+            if (Actual < Total)
+            {
+                Actual++;
+                HeroeActual = heroes[Actual-1];
+            }
+        }
+
+        public void Anterior()
+        {
+            if (Actual > 1)
+            {
+                Actual--;
+                HeroeActual = heroes[Actual-1];
+            }
+        }
+    }
+}
